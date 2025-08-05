673ad51ae717e599b03a6d988b0713ff77761494
darshan <darshan.paccha@gmail.com>
2025-08-06 00:32:55 +0530

    feat: update schema to satisfy many to many relation for users & boards

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -5,27 +5,20 @@ import {
   uuid,
   index,
   uniqueIndex,
+  pgEnum,
 } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   firstName: varchar("first_name", { length: 255 }).notNull(),
-  lastName: varchar("first_name", { length: 255 }).notNull(),
+  lastName: varchar("last_name", { length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
 });
 
-// one to many relation - users to boards
-export const boardsTable = pgTable(
-  "boards",
-  {
-    id: uuid("id").primaryKey().defaultRandom(),
-    title: varchar("title", { length: 255 }).notNull(),
-    userId: uuid("user_id")
-      .references(() => usersTable.id, { onDelete: "cascade" })
-      .notNull(),
-  },
-  (t) => [index("user_id_index").on(t.userId)] // we are not using uniqueIndex cause many boards can have the same userId
-);
+export const boardsTable = pgTable("boards", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  title: varchar("title", { length: 255 }).notNull(),
+});
 
 // one to many relation - boards to lists
 export const listsTable = pgTable(
@@ -38,7 +31,7 @@ export const listsTable = pgTable(
       onDelete: "cascade",
     }),
   },
-  (t) => [index("board_id_index").on(t.boardId)]
+  (t) => [index("board_id_index").on(t.boardId)] // we are not using uniqueIndex cause many lists can have the same boardId
 );
 
 // one to many relation - lists to cards
@@ -47,10 +40,28 @@ export const cardsTable = pgTable(
   {
     id: uuid("id").primaryKey().defaultRandom(),
     title: varchar("title", { length: 255 }).notNull(),
-    postition: integer("position").notNull(),
+    position: integer("position").notNull(),
     listId: uuid("list_id").references(() => listsTable.id, {
       onDelete: "cascade",
     }),
   },
   (t) => [index("list_id_index").on(t.listId)]
+);
+
+export const rolesEnum = pgEnum("roles", ["GUEST", "ADMIN"]);
+
+// many to many relation - users & boards
+export const boardMembersTable = pgTable(
+  "board_members",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    userId: uuid("user_id").references(() => usersTable.id, {
+      onDelete: "cascade",
+    }),
+    boardId: uuid("board_id").references(() => boardsTable.id, {
+      onDelete: "cascade",
+    }),
+    role: rolesEnum().default("GUEST"),
+  },
+  (t) => [uniqueIndex("user_id_board_id_index").on(t.userId, t.boardId)]
 );
