37cb47fc0f7b7fb36dfaa68a07ce4179bfb3234f
darshan <darshan.paccha@gmail.com>
2025-08-08 01:00:51 +0530

    fix: add key prop to link in board card

--- components/board-cards.tsx
+++ components/board-cards.tsx
@@ -17,8 +17,8 @@ export function BoardCards() {
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       {boards.map((board: Board) => (
-        <Link href={`/boards/${board.id}`}>
-          <Card className="@container/card cursor-pointer" key={board.id}>
+        <Link href={`/boards/${board.id}`} key={board.id}>
+          <Card className="@container/card cursor-pointer">
             <CardHeader>
               <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
                 {board.title}
