2d60e04e5a1ff98e9450a67a486c5aa57a1f2bca
darshan <darshan.paccha@gmail.com>
2025-08-09 10:58:48 +0530

    feat: add /api/lists get route

--- app/api/lists/route.ts
+++ app/api/lists/route.ts
@@ -1,7 +1,9 @@
+import { NextResponse } from "next/server";
+import { eq } from "drizzle-orm";
+
 import { db } from "@/lib/db";
 import { listsTable } from "@/lib/db/schema";
 import { currentUser, isABoardMember } from "@/lib/server-utils";
-import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
@@ -16,7 +18,7 @@ export async function POST(req: Request) {
     const isMember = await isABoardMember(user.id, boardId);
 
     if (!isMember) {
-      return new NextResponse("Forbidden", { status: 404 });
+      return new NextResponse("Forbidden", { status: 403 });
     }
 
     const [list] = await db
@@ -31,6 +33,40 @@ export async function POST(req: Request) {
     return NextResponse.json(list);
   } catch (error) {
     console.log("[LISTS_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function GET(req: Request) {
+  try {
+    const user = await currentUser();
+
+    if (!user) {
+      return new NextResponse("Not Authorized", { status: 401 });
+    }
+
+    const { searchParams } = new URL(req.url);
+
+    const boardId = searchParams.get("boardId");
+
+    if (!boardId) {
+      return new NextResponse("Board Id Missing", { status: 400 });
+    }
+
+    const isMember = await isABoardMember(user.id, boardId);
+
+    if (!isMember) {
+      return new NextResponse("Forbidden", { status: 403 });
+    }
+
+    const lists = await db
+      .select()
+      .from(listsTable)
+      .where(eq(listsTable.boardId, boardId));
+
+    return NextResponse.json(lists);
+  } catch (error) {
+    console.log("[LISTS_GET]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
